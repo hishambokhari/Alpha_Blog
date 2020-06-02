d3af695e6c4951e2e7f3fa4fb9388a2f53572e37
Hisham Bokhari <hisham.bokhari@cloudasset.com>
2020-06-02 15:49:09 +0500

    Add: Flash msgs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ActionController::Base
+class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
@@ -9,12 +9,16 @@ class ArticlesController < ActionController::Base
   end
 
   def new
-    
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+    redirect_to @article  
+    else
+      render 'new'
+    end
   end
 end
